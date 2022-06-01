33b07f62ad64edc60dcc8383866607f4d4cac444
Jonas Gann <jonas@gann.digital>
2022-06-01 15:09:25 +0200

    finish ex05

--- ex05/Sprite.gd
+++ ex05/Sprite.gd
@@ -1,6 +1,6 @@
 extends Sprite
 
-var shad = material.get_shader_param("green")
+var direction = Vector2.UP
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,9 +8,9 @@ var shad = material.get_shader_param("green")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	material.get_.g = 0.0
+	material.set_shader_param("green", 0.0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
