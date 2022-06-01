09f34f89103d01ac0ed4fe9faa1765b18daddc28
Timo <Timoege@online.de>
2022-06-01 13:23:37 +0200

    uebung05 unfertig

---
+++ ex05/main.gd
@@ -0,0 +1,16 @@
+extends Sprite
+
+var shad = material.get_shader_param("green")
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	material.get_.g = 0.0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
